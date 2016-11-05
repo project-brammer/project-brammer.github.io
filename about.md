586559b77fd9c21081847237a34260f625aa9678
Jeff Herrera <jeffreyherrera@Jeffreys-MacBook-Pro.local>
2016-11-05 16:54:52 -0400

    Initial Beta Commit

---
+++ about.md
@@ -0,0 +1,17 @@
+---
+layout: content-page
+title: About | John Paul Brammer
+permalink: /about/
+---
+<a href="#" class="about-header">
+    Download Media Kit <i class="fa fa-download" aria-hidden="true"></i>
+</a>
+
+<div class="skinny-row">
+    <div class="longform-description">
+        {{ site.longform-description }}
+        <br><br>
+        Find me on <a href="http://www.twitter.com/{{ site.twitter }}" target="_blank">Twitter</a>
+    </div>
+</div>
+
---
+++ assets/js/base.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+  $('.menu-button').sidr();
+  $('.close-button').sidr('close', 'sidr-main');
+  if ( $('.meta-stick').is(":visible")){
+    stickyMeta();
+  }
+  socialShare();
+  headerScroll();
+  filterButtons();
+});
+
+// FUNCTIONS
+
+function socialShare(){
+  $(".js-social-share").on("click", function(e) {
+  e.preventDefault();
+
+  windowPopup($(this).attr("href"), 500, 300);
+  });
+  var jsSocialShares = document.querySelectorAll(".js-social-share");
+  if (jsSocialShares) {
+    [].forEach.call(jsSocialShares, function(anchor) {
+      anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        windowPopup(this.href, 500, 300);
+      });
+    });
+  }
+}
+
+function headerScroll(){
+  $(window).scroll(function() {
+  var siteHeader = $('#site-header-container')
+  var siteFooter = $('#site-footer-container')
+  if ($(this).scrollTop() > 1){  
+      siteHeader.addClass("sticky");
+    }
+    else{
+      siteHeader.removeClass("sticky");
+    }
+  });
+}
+
+function windowPopup(url, width, height) {
+  // Calculate the position of the popup so
+  // it’s centered on the screen.
+  var left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+
+  window.open(
+    url,
+    "",
+    "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
+  );
+}
+
+function filterButtons(){
+  var $btns = $('.filter-buttons').click(function() {
+  if (this.id == 'all') {
+    $('.post').show();
+  } else {
+    var $el = $('.' + this.id).show();
+    $('.post').not($el).hide();
+  }
+  $btns.removeClass('active');
+  $(this).addClass('active');
+  })
+}
+
+function stickyMeta(){
+  $(window).scroll(function() {
+    var Meta = $('.meta-stick')
+    var TopPos = $(this).scrollTop()
+    if (TopPos > 1) {  
+        Meta.css('top', TopPos + "px" )
+      }
+    else if(TopPos < 1) {
+      Meta.css('top','18px');
+    }
+    else{
+      Meta.css('top','18px');
+    }
+  });
+
+  $(window).scroll(function(){
+    if ( ($(document).height() - $(window).height()) - $(window).scrollTop() < 150 ){
+      $('.meta-stick').fadeOut(300);
+    }
+    else {
+      $('.meta-stick').fadeIn(300);
+    }
+  });
+}
